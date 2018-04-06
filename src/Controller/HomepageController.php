19a57eef199ca48d1abf018292b71d8602a2e9c4
Damian <damianchojnacki"op.pl>
2018-04-06 09:32:28 +0200

    first controller

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends Controller
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(){
+        return $this -> render('homepage.html.twig', array(
+            'number' => mt_rand(0, 100),
+        ));
+    }
+}
